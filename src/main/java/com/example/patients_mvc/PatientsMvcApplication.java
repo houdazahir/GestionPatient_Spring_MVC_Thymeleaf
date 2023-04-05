fec199519fbff58a62a4cbc7d8b534c3021a63f0
houda zahir <zahir.houda11@gmail.com>
2023-04-05 21:50:39 +0000

    first commit

---
+++ src/main/java/com/example/patients_mvc/PatientsMvcApplication.java
@@ -0,0 +1,31 @@
+package com.example.patients_mvc;
+
+import com.example.patients_mvc.entities.Patient;
+import com.example.patients_mvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientsMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsMvcApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
+        return args -> {
+            patientRepository.save(new Patient(null, "Houda", new Date(), true, 100));
+            patientRepository.save(new Patient(null, "Mohamed", new Date(), false, 150));
+            patientRepository.save(new Patient(null, "Selma", new Date(), false, 80));
+            patientRepository.save(new Patient(null, "Ali", new Date(), true, 450));
+            patientRepository.findAll().forEach(p -> {
+                System.out.println(p.getNom());
+            });
+        };
+    }
+}
---
+++ src/main/java/com/example/patients_mvc/repositories/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.example.patients_mvc.repositories;
+
+import com.example.patients_mvc.entities.Patient;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository  extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContains(String kw, Pageable pageable);
+
+}
